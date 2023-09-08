2f434ebb4df00130f683f9382a9430bfeac81628
Abdullah Khamoor <abdullah.khamoor4@gmail.com>
2023-09-08 15:08:49 +0500

    Assignment 2

---
+++ app.js
@@ -0,0 +1,151 @@
+//Todo Chapter no 5
+
+//? Q1
+
+// var a = 3;
+// var b = 5;
+// var c = a + b;
+// document.write("Sum of " + a + " and "+ b + " is " +8 + "<Br>");
+
+ //? Q2
+
+// var d = a - b;
+// document.write("Subtraction " + a + " and "+ b + " is " + 8 + "<Br>" );
+// var e = a * b;
+// document.write("Multiplication " + a + " and "+ b + " is " + 8 + "<Br>" );
+
+// var f = a / b;
+// document.write("Division " + a + " and "+ b + " is " +8 + "<Br>");
+// var g = a % b;
+// document.write("Modulus " + a + " and "+ b + " is " +8 + "<Br>");
+
+//? Q3
+
+//! a
+// var a1 = 2;
+//! b 
+// document.write("Value after declaration is : "+ a1 + "<Br>");
+//! c
+// var a2 = 5
+//! d
+// document.write("Value after initialization is : "+ a2+ "<Br>");
+//! e
+// var a2 = ++a2 ;
+//! f
+// document.write("Value after increment is : "+ a2 + "<Br>");
+//! g
+// var a2 = a2 + 7;
+//! h
+// document.write("Value after Addition is : "+ a2 + "<Br>");
+//! i
+// var a2 = --a2;
+//! j
+// document.write("Value after Addition is : "+ a2 + "<Br>");
+//! k
+// var a2 = a2 % 3;
+//! l
+
+//?=========== Q4 ============
+
+// var ticketPrice = 600 * 5;
+// document.write("Total cost to buy 5 tickets to a movie is " + ticketPrice);
+
+//?==========  Q6  ==========
+
+//! a
+// var Fahrenheit = 25;
+//! b
+// Fahrenheit = (25*9/5) + 32;
+// document.write("25°C is "+ Fahrenheit +"°F" + "<br>"  );
+//! c
+// var Fahrenheit = 70;
+//!d
+// celsius = (70 - 32)* 5/9;
+// document.write("70°F is "+ celsius+"°F");
+
+//?=========  Q7  ======
+
+//!
+// var item1 = 650;
+// var quantity = 3;
+// var item2 = 100;
+// var quantity2 = 7;
+// var charges = 100;
+// var totalCost = 2750;
+//! a
+// document.write("Price of item 1 is "+ item1 + "<br>");
+//! b
+// document.write("Quantity of item 1 is "+ quantity + "<br>");
+//! c
+// document.write("Price of item 2 is "+ item2 + "<br>");
+//! d
+// document.write("Quantity of item 2 is "+ quantity2 + "<br>");
+//! e
+// document.write("Shipping Charges "+ charges + "<br>");
+//! total cost
+// document.write("Total Cost of your order is  " + totalCost + "<br>");   
+
+
+//?========== Q8 ================
+
+// var totalMark = 980;
+// var markObtained = 804;
+// var Percentage = (804/980)*100 ;
+// document.write("Total mark : "+ totalMark + "<br>");
+// document.write("Marks obtained : "+ markObtained + "<br>");
+// document.write("Percentage : "+ Percentage + "<br>");
+
+
+//?===========  Q9  =====================
+
+// var us_Currency = 306.69;
+// var saudi_Currency = 81.76;
+// var total_Currency = (us_Currency*10)+(saudi_Currency*25);
+// document.write("Total Currency in PKR: "+total_Currency);
+
+//?===============  Q10  ==================
+
+// var num = 4;
+// var num2 = 4+5*10/2;
+// console.log(num2);
+
+
+//?===========  Q11  ======================
+
+// var currentYear = 2023;
+// var birthYear = 2002;
+// var yourAge = currentYear-birthYear;
+// document.write("Current Year: "+ currentYear+ "<br>" );
+// document.write("Birth Year: " + birthYear + "<br>");
+// document.write("Your Age is: "+ yourAge + "<br>");
+
+//?==================   Q12  ====================
+//! a
+// var radius = 25;
+//! b
+// var circumferance = 2*3.142*25;
+// var area = 3.142*25*25;
+// document.write("Radius of a circle: "+ radius + "<br>");
+// document.write("The circumference is: "+ circumferance + "<br>");
+// document.write("The area is: "+ area + "<br>");
+
+//?====================  Q13  =====================
+//! a
+// var favouriteSnack = "Chocalato";
+//! b
+// var currentAge = "20";
+//! c
+// var maxAge = "65";
+//! d
+// var estimateSnackPerDay = "2";
+//! e
+// var totalEatRestLife = (65-20)*2;
+
+// document.write("Favourite Snack: "+ favouriteSnack+ "<br>");
+// document.write("Current age: "+ currentAge+ "<br>");
+// document.write("Estimated Maximum Age: "+ maxAge+ "<br>");
+// document.write("Amount of snack per day: "+ estimateSnackPerDay+ "<br>");
+// document.write("You will need "+ totalEatRestLife+ " Chocalato" + " to last you until the ripe old age of "+maxAge +"<br>");
+  
+
+
